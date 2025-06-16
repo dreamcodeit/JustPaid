41d1f13baa017c908d73114c696af04e04ca1e68
dreamcodeit <contact@dreamcodeit.com>
2025-06-16 11:16:36 +0530

    first commit

---
+++ src/app/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <>
+    <div className='container'>
+<div className='logo'> 
+<h2>logo</h2>
+</div>
+      
+    </div>
+    </>
+  )
+}
+
+export default header
---
+++ src/app/platform-receivable/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Platform Receivable</div>
+  )
+}
+
+export default page
