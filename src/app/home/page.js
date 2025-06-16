254fb5431130d2a3043e708ba6a328b755220cae
dreamcodeit <contact@dreamcodeit.com>
2025-06-16 14:15:58 +0530

    images log

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -9,7 +9,7 @@ const page = () => {
           <div className='flex justify-between'>
             <div className='w-[48%] py-[50px]'>
 
-                <span className='rounded-[4px] gap-[4px] px-[12] py-[5] logo-section flex  bg-[#EAE6DC]'> <span>Backed by</span> <Image src={'images/y-log.svg'} height={20} width={20} alt='img'></Image> <span>Combinator (W23)</span> </span>
+                <span className='rounded-[4px] gap-[4px] px-[12] py-[5] logo-section flex  bg-[#EAE6DC]'> <span>Backed by</span> <Image src={'/public/images/y-log.svg'} height={20} width={20} alt='img'></Image> <span>Combinator (W23)</span> </span>
 
               <h1 className='mt-[32px] text-[48px]'>Your autonomous <b>AI billing agent </b>for complex financial operations</h1>
 
